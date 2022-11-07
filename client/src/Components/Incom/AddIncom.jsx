eb81da394f8b9434e3057e9b17166b8a9d241aa4
Julihumere <julihumereuner@gmail.com>
2022-11-07 16:16:28 -0300

    add calendar

--- client/src/Components/Incom/AddIncom.jsx
+++ client/src/Components/Incom/AddIncom.jsx
@@ -5,19 +5,33 @@ import { useNavigate } from "react-router-dom";
 import { addIncom } from "../../Redux/Actions";
 import Cookies from "universal-cookie";
 import Swal from "sweetalert2";
+import { AiOutlineCalendar } from "react-icons/ai";
+import { Calendar } from "react-calendar";
 export default function AddIncom() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const cookies = new Cookies();
   const cookieEmail = cookies.get("email");
+  const [formatDate] = useState();
+  const [date, setDate] = useState(new Date());
   const [incom, setIncom] = useState({
     concept: "",
     amount: "",
     category: "",
-    date: "",
+    date: date.toLocaleDateString(),
     email: cookieEmail,
   });
   const [error, setError] = useState({});
+
+  const [calendar, setCalendar] = useState(false);
+
+  const onChangeDate = (e) => {
+    setDate(e);
+    setIncom({
+      ...incom,
+      date: e.toLocaleDateString(),
+    });
+  };
 
   const onChange = (e) => {
     setIncom({
@@ -25,6 +39,9 @@ export default function AddIncom() {
       [e.target.name]: e.target.value,
     });
   };
+
+  console.log(date);
+  console.log(incom);
 
   const validate = (input) => {
     let errors = {};
@@ -39,10 +56,6 @@ export default function AddIncom() {
     }
     if (!input.date) {
       errors.date = "Date is required";
-    } else if (
-      !/^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})$/.test(input.date)
-    ) {
-      errors.date = "Date must be dd/mm/yyyy";
     }
     return errors;
   };
@@ -106,15 +119,29 @@ export default function AddIncom() {
         <div className="AddIncom__label_input">
           {" "}
           <label>Date</label>
+          {/* <div className="AddIncom__input__calendar"> */}
           <input
             type="text"
             name="date"
             value={incom.date}
             onChange={onChange}
-          />
+            onClick={setCalendar}
+            readOnly
+          />{" "}
+          {calendar && (
+            <Calendar
+              value={date}
+              onChange={onChangeDate}
+              className="calendar"
+              maxDate={new Date()}
+            />
+          )}
+          {/* </div> */}
           {error.date && <p>{error.date}</p>}
         </div>
-        <button type="submit">Create!</button>
+        <button className="AddIncom__button_submit" type="submit">
+          Create!
+        </button>
       </form>
     </div>
   );
